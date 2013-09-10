c7e9946069d39c3ea5dd6a635151f101a2bc2f59
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-09-10 11:55:03 +0200

    Lint (top-scope variables)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,30 +1,30 @@
 class syslog-ng {
 
-  case $operatingsystem {
-    Debian: { $ostmpl = "syslog-ng/syslog-ng.debian.conf.erb" }
-    RedHat:  { $ostmpl = "syslog-ng/syslog-ng.redhat.conf.erb" }
-    default: { fail "Unsupported operatingsystem ${operatingsystem}" }
+  case $::osfamily {
+    Debian: { $ostmpl = 'syslog-ng/syslog-ng.debian.conf.erb' }
+    RedHat:  { $ostmpl = 'syslog-ng/syslog-ng.redhat.conf.erb' }
+    default: { fail "Unsupported OS family ${::osfamily}" }
   }
 
-  package {"syslog-ng":
+  package {'syslog-ng':
     ensure => installed,
   }
 
-  service {"syslog-ng":
+  service {'syslog-ng':
     ensure    => running,
     enable    => true,
-    subscribe => File["/etc/syslog-ng/syslog-ng.conf"],
-    require   => Package["syslog-ng"],
+    subscribe => File['/etc/syslog-ng/syslog-ng.conf'],
+    require   => Package['syslog-ng'],
   }
 
-  file {"/etc/syslog-ng/syslog-ng.conf":
+  file {'/etc/syslog-ng/syslog-ng.conf':
     ensure  => present,
     content => template(
-      "syslog-ng/syslog-ng.header.conf.erb",
-      "syslog-ng/syslog-ng.client-options.conf.erb",
+      'syslog-ng/syslog-ng.header.conf.erb',
+      'syslog-ng/syslog-ng.client-options.conf.erb',
       $ostmpl,
-      "syslog-ng/syslog-ng.client-sourcedest.conf.erb"),
-    require => Package["syslog-ng"],
-    notify  => Service["syslog-ng"],
+      'syslog-ng/syslog-ng.client-sourcedest.conf.erb'),
+    require => Package['syslog-ng'],
+    notify  => Service['syslog-ng'],
   }
 }
